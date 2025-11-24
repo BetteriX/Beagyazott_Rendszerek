627a626ca242ec7eab9bc846058a6058bd6558c9
BetteriX <szabozsolt430@gmail.com>
2025-11-24 11:53:09 +0100

    10.ora

---
+++ 10/1.A.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import time
+import cv2
+
+
+def main():
+    image_path = "jeep.jpg"
+    label_path = "../imagenet1000_labels.txt"
+    prototxt_path = "../googlenet.prototxt.txt"
+    model_path = "../googlenet.caffemodel"
+
+    image = cv2.imread(image_path)
+    rows = open(label_path).read().strip().split("\n")
+    classes = [r.split(",")[0] for r in rows]
+
+    blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
+
+    print("Loading model...")
+    net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+
+    net.setInput(blob)
+    start = time.time()
+    preds = net.forward()
+    end = time.time()
+    print("Classification time: {:.5} seconds".format(end - start))
+
+    idxs = np.argsort(preds[0])[::-1][:5]
+
+    for i, idx in enumerate(idxs):
+        if i == 0:
+            text = "Label: {}, {:.2f}%".format(classes[idx], preds[0][idx] * 100)
+            cv2.putText(
+                image, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2
+            )
+        print(
+            "{}. label: {}, probability: {:.5}".format(
+                i + 1, classes[idx], preds[0][idx]
+            )
+        )
+
+    output_image = image_path.replace(".jpg", "_labeled.jpg")
+    cv2.imwrite(output_image, image)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 10/2.A.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2
+
+from imutils.video import VideoStream
+import imutils
+import time
+
+
+def main():
+    prototxt_path = "../MobileNetSSD_deploy_prototxt.txt"
+    model_path = "../MobileNetSSD_deploy.caffemodel"
+    video_path = 0
+
+    conf_limit = 0.25
+
+    CLASSES = [
+        "background",
+        "aeroplane",
+        "bicycle",
+        "bird",
+        "boat",
+        "bottle",
+        "bus",
+        "car",
+        "cat",
+        "chair",
+        "cow",
+        "diningtable",
+        "dog",
+        "horse",
+        "motorbike",
+        "person",
+        "pottedplant",
+        "sheep",
+        "sofa",
+        "train",
+        "tv/monitor",
+    ]
+
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+    vs = VideoStream(src=video_path).start()
+    time.sleep(2)
+
+    print("Loading model...")
+    net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+
+    while True:
+        frame = vs.read()
+        if frame is None:
+            continue
+
+        frame = imutils.resize(frame, width=600)
+        # image = cv2.imread(image_path)
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(
+            cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5
+        )
+
+        # print("Sending image through the network...")
+        net.setInput(blob)
+        detections = net.forward()
+
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_limit:
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+                # print("{}".format(label))
+
+                cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(
+                    frame,
+                    label,
+                    (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    COLORS[idx],
+                    2,
+                )
+        cv2.imshow("Camera image", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    vs.stop()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
